9f04c4d75163811a34c2f815dca7631797f4ec64
jayaprasad356 <jayaprasad356@gmail.com>
2022-08-16 10:12:04 +0530

    minor bug fixed

---
+++ app/src/main/java/com/gm/brainobrain/activities/RemainderActivity.java
@@ -0,0 +1,50 @@
+package com.gm.brainobrain.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import com.gm.brainobrain.R;
+
+import java.util.Calendar;
+
+public class RemainderActivity extends AppCompatActivity {
+    TimePicker edit_alarm_time_picker;
+    Button btnSetReminder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remainder);
+        edit_alarm_time_picker = findViewById(R.id.edit_alarm_time_picker);
+        btnSetReminder = findViewById(R.id.btnSetReminder);
+        btnSetReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("REMINDER_TIME",getTimePickerMinute(edit_alarm_time_picker) +" - "+getTimePickerHour(edit_alarm_time_picker));
+
+            }
+        });
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public static int getTimePickerMinute(TimePicker picker) {
+        return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                ? picker.getMinute()
+                : picker.getCurrentMinute();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public static int getTimePickerHour(TimePicker picker) {
+        return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                ? picker.getHour()
+                : picker.getCurrentHour();
+    }
+}
---
+++ app/src/main/java/com/gm/brainobrain/service/AlarmReceiver.java
@@ -0,0 +1,92 @@
+package com.gm.brainobrain.service;
+
+import static android.app.NotificationManager.IMPORTANCE_HIGH;
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.KITKAT;
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static android.os.Build.VERSION_CODES.O;
+import android.app.AlarmManager;
+import android.app.AlarmManager.AlarmClockInfo;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import com.gm.brainobrain.R;
+import com.gm.brainobrain.model.Alarm;
+
+import java.util.Calendar;
+import java.util.List;
+
+public final class AlarmReceiver extends BroadcastReceiver {
+
+    private static final String TAG = AlarmReceiver.class.getSimpleName();
+    private static final String CHANNEL_ID = "alarm_channel";
+
+    private static final String BUNDLE_EXTRA = "bundle_extra";
+    private static final String ALARM_KEY = "alarm_key";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        final Alarm alarm = intent.getBundleExtra(BUNDLE_EXTRA).getParcelable(ALARM_KEY);
+        if(alarm == null) {
+            Log.e(TAG, "Alarm is null", new NullPointerException());
+            return;
+        }
+
+        final int id = alarm.notificationId();
+
+        final NotificationManager manager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        createNotificationChannel(context);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.bluecircle);
+        builder.setColor(ContextCompat.getColor(context, R.color.primaryColor));
+        builder.setContentTitle(context.getString(R.string.app_name));
+        builder.setContentText(alarm.getLabel());
+        builder.setTicker(alarm.getLabel());
+        builder.setVibrate(new long[] {1000,500,1000,500,1000,500});
+        builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+        //builder.setContentIntent(launchAlarmLandingPage(context, alarm));
+        builder.setAutoCancel(true);
+        builder.setPriority(Notification.PRIORITY_HIGH);
+
+        manager.notify(id, builder.build());
+
+
+    }
+
+
+    private static void createNotificationChannel(Context ctx) {
+        if(SDK_INT < O) return;
+
+        final NotificationManager mgr = ctx.getSystemService(NotificationManager.class);
+        if(mgr == null) return;
+
+        final String name = ctx.getString(R.string.app_name);
+        if(mgr.getNotificationChannel(name) == null) {
+            final NotificationChannel channel =
+                    new NotificationChannel(CHANNEL_ID, name, IMPORTANCE_HIGH);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[] {1000,500,1000,500,1000,500});
+            channel.setBypassDnd(true);
+            mgr.createNotificationChannel(channel);
+        }
+    }
+
+}
